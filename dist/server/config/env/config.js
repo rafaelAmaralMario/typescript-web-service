7c2a0d1358fd46aa2cf4c0d8d25283f4cfa69787
rafaelmario91 <rafael.mario@objectedge.com>
2020-07-24 10:21:27 -0300

    removing autogenerated files

--- dist/server/config/env/config.js
+++
@@ -1,5 +0,0 @@
-var extension = "js";
-if (process.env.NODE_ENV === "dev") {
-    extension = "ts";
-}
-module.exports = function () { return require("./" + process.env.NODE_ENV + ".env." + extension); };
